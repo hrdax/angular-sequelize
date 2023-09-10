72002182db7875a357f4ff85ba2b12dff9aee56f
hrdax <juanpa_200018@hotmail.com>
2023-09-10 16:06:40 -0300

    validator form, addproduct

--- app-productos/src/app/components/add-edit-product/add-edit-product.component.ts
+++ app-productos/src/app/components/add-edit-product/add-edit-product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-add-edit-product',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-edit-product.component.css']
 })
 export class AddEditProductComponent {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      price: ['', Validators.required],
+      stock: ['', Validators.required],
+    })
+  }
+  addProduct(){
+    console.log('alo')
+  }
 
 }
