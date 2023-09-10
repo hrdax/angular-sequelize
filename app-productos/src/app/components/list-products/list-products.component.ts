6922eb65198d60c24c8258630156b5c243f62461
hrdax <juanpa_200018@hotmail.com>
2023-09-10 15:29:35 -0300

    creando maquetado de lista

--- app-productos/src/app/components/list-products/list-products.component.ts
+++ app-productos/src/app/components/list-products/list-products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/interfaces/product';
 
 @Component({
   selector: 'app-list-products',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-products.component.css']
 })
 export class ListProductsComponent {
+  listProducts: Product[] = [
+    { name: 'Coca Cola', description: 'Bebida', price: 800, stock: 5 },
+    { name: 'Cola Cao', description: 'Cereal con Chocolate', price: 1000, stock: 10 },
+
+  ]
 
 }
