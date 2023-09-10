28d1b806639e118a13db45967bce9d432bb96741
hrdax <juanpa_200018@hotmail.com>
2023-09-10 17:40:32 -0300

    delete, post, get api

--- server/dist/src/controllers/product.js
+++ server/dist/src/controllers/product.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getProduct = exports.getProducts = void 0;
+exports.postProduct = exports.deleteProduct = exports.getProduct = exports.getProducts = void 0;
 const getProducts = (req, res) => {
     res.json({
         msg: 'get products'
@@ -9,7 +9,23 @@ const getProducts = (req, res) => {
 exports.getProducts = getProducts;
 const getProduct = (req, res) => {
     res.json({
-        msg: 'get product'
+        msg: 'get product',
+        id: req.params.id
     });
 };
 exports.getProduct = getProduct;
+const deleteProduct = (req, res) => {
+    res.json({
+        msg: 'delete product',
+        id: req.params.id
+    });
+};
+exports.deleteProduct = deleteProduct;
+const postProduct = (req, res) => {
+    const { body } = req;
+    res.json({
+        msg: 'delete product',
+        body
+    });
+};
+exports.postProduct = postProduct;
--- server/src/controllers/product.ts
+++ server/src/controllers/product.ts
@@ -8,6 +8,22 @@ export const getProducts = (req: Request, res: Response) => {
 
 export const getProduct = (req: Request, res: Response) => {
     res.json({
-        msg: 'get product'
+        msg: 'get product',
+        id: req.params.id
+    })
+}
+
+export const deleteProduct = (req: Request, res: Response) => {
+    res.json({
+        msg: 'delete product',
+        id: req.params.id
+    })
+}
+
+export const postProduct = (req: Request, res: Response) => {
+    const { body } = req
+    res.json({
+        msg: 'delete product',
+        body
     })
 }
