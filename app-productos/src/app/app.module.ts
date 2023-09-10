cc858b0acda74bfe90a6f2b53e35735e261cd953
hrdax <juanpa_200018@hotmail.com>
2023-09-10 16:06:53 -0300

    reactiveform module

--- app-productos/src/app/app.module.ts
+++ app-productos/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -16,7 +18,8 @@ import { AddEditProductComponent } from './components/add-edit-product/add-edit-
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
