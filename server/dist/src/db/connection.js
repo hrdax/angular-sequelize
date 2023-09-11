ccd0c5c08a24d31d5f133e9085e3d1fafbbd0b9d
hrdax <juanpa_200018@hotmail.com>
2023-09-11 18:10:39 -0300

    porsac

--- server/dist/src/db/connection.js
+++ server/dist/src/db/connection.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize('almacen', 'postgres', '1234', {
+const sequelize = new sequelize_1.Sequelize('almacen', '', '', {
     host: 'localhost',
     dialect: 'postgres'
 });
