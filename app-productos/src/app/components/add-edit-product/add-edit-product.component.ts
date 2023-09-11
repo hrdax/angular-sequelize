7fc519015c8071434a230bc0a67c569cbbf0b5b1
hrdax <juanpa_200018@hotmail.com>
2023-09-11 17:42:33 -0300

    agregar producto

--- app-productos/src/app/components/add-edit-product/add-edit-product.component.ts
+++ app-productos/src/app/components/add-edit-product/add-edit-product.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { Router } from '@angular/router';
 import { Product } from 'src/app/interfaces/product';
+import { ProductService } from 'src/app/services/product.service';
+
 
 @Component({
   selector: 'app-add-edit-product',
@@ -9,8 +12,8 @@ import { Product } from 'src/app/interfaces/product';
 })
 export class AddEditProductComponent {
   form: FormGroup;
-
-  constructor(private fb: FormBuilder) {
+  
+  constructor(private fb: FormBuilder, private _productService: ProductService, private router: Router) {
     this.form = this.fb.group({
       name: ['', Validators.required],
       description: ['', Validators.required],
@@ -25,8 +28,8 @@ export class AddEditProductComponent {
       price: this.form.value.price,
       stock: this.form.value.stock,
     }
-    console.log(product)
-    
+    this._productService.saveProduct(product).subscribe(() => {})
+    this.router.navigate(['/'])
 
   }
 
