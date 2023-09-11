1f52b3faafef971df018758b6a826e39f53f6e1f
hrdax <juanpa_200018@hotmail.com>
2023-09-11 17:12:08 -0300

    httpclient module

--- app-productos/src/app/app.module.ts
+++ app-productos/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +20,8 @@ import { AddEditProductComponent } from './components/add-edit-product/add-edit-
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
