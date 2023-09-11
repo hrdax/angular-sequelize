cf2310fec5d4e9da1dc184c0b73d6655213b3563
hrdax <juanpa_200018@hotmail.com>
2023-09-11 17:14:13 -0300

    little detail

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize"
 
-const sequelize = new Sequelize('almacen', 'postgres', '1234', {
+const sequelize = new Sequelize('almacen', '', '', {
     host: 'localhost',
     dialect: 'postgres'
 });
